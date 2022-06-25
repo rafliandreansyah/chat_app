f67bf1aeece36390f8aaa2cd0434e1532632b502
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2022-06-25 11:35:44 +0700

    setup firebase and add stream

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ChateScreen extends StatelessWidget {
-  const ChateScreen({Key? key}) : super(key: key);
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _chatSnapshot = FirebaseFirestore.instance
+      .collection('chats/auwFUep7zxbKGUAsR0zI/messages')
+      .snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +20,27 @@ class ChateScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat Screen'),
       ),
-      body: Center(
-        child: Text('Chat Screen'),
+      body: StreamBuilder(
+          stream: _chatSnapshot,
+          builder: (ctx, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final listData = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: listData.length,
+              itemBuilder: (ctx, i) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(listData[i]['text']),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
